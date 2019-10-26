e17eebf2c3c0df56cb57e2e3dee80440f4d59209
Alexey Dolzhenko <tooroff@gmail.com>
2019-10-26 11:39:34 +0700

    Установлен и настроен mangoose

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
 const users = require('./routes/users');
 const cards = require('./routes/cards');
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
