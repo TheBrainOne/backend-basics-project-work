958f7504a2f4d774dc39ea5fe79b50c323132969
Alexey Dolzhenko <tooroff@gmail.com>
2019-10-29 14:37:29 +0700

    Добавлены проверка на возможность удаления только своей карточки
    Для мидлвэра добавлено исключение линтера

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 
+// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   if (!req.cookies.jwt) {
     return res.status(401).send({ message: 'Необходима авторизация!' });
