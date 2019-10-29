ad71610c900c3b00b9c176b30faed8cf8e819add
Alexey Dolzhenko <tooroff@gmail.com>
2019-10-29 11:18:58 +0700

    Создан роут для логина и регистрации

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 const users = require('./routes/users');
 const cards = require('./routes/cards');
+const { login, createUser } = require('./controllers/users');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -24,6 +25,9 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/login', login);
+app.post('/signup', createUser);
 
 app.use('/users', users);
 app.use('/cards', cards);
