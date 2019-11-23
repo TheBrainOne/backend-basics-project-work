5c54260adbb902703e00400be9ceb4ac1d36bf75
Alexey Dolzhenko <tooroff@gmail.com>
2019-11-23 20:53:26 +0700

    Валидация приходящих на сервер данных

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -10,9 +11,9 @@ const app = express();
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const notFound = require('./middlewares/not-found');
 const error = require('./middlewares/error');
+const auth = require('./middlewares/auth');
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
@@ -23,8 +24,21 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required(),
+  }),
+}), createUser);
 
 app.use(auth);
 
@@ -32,6 +46,7 @@ app.use('/users', users);
 app.use('/cards', cards);
 
 app.use(notFound);
+app.use(errors());
 app.use(error);
 
 app.listen(PORT, () => {
